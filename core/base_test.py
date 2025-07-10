c7872df9fca68c69a8843066e5b6ade749fc2eef
igorBys <stevejobs69@MacBook-Air-steve.local>
2025-07-10 19:56:55 +0300

    /

--- core/base_test.py
+++ core/base_test.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 def browser():
     options = webdriver.ChromeOptions()
     options.add_argument('--lang=ru')
-    driver = webdriver.Remote(command_executor="https://5.35.85.102:4444", options=options)
+    driver = webdriver.Remote(command_executor="http://5.35.85.102:4444", options=options)
     yield driver
     if driver:
         driver.quit()
